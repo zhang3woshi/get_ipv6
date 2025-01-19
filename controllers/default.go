38d3fbed50611e6a02c6716f162f137d8647f01d
zhang3woshi <liesun4402@gmail.com>
2025-01-19 21:36:33 +0800

    feat:1.增加只获取128位ipv6地址

--- controllers/default.go
+++ controllers/default.go
@@ -18,12 +18,26 @@ type MainController struct {
 // @Success 200 {string} success
 // @router / [get]
 func (c *MainController) Get() {
+	var ipv6 string
 	cmd := "ip -6 addr show dev pppoe-wan | grep 'inet6' | grep 'global' | awk '{print $2}' | cut -d/ -f1"
 	out, err := exec.Command("sh", "-c", cmd).Output()
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-	ipv6 := strings.TrimSpace(string(out))
+	// ipv6 := strings.TrimSpace(string(out))
+	// c.Ctx.WriteString(ipv6)
+	// 将输出按行分割成数组
+	addresses := strings.Split(strings.TrimSpace(string(out)), "\n")
+
+	// 过滤出长度为 128 位的 IPv6 地址
+	for _, addr := range addresses {
+		if strings.Count(addr, ":") == 7 { // IPv6 地址应包含 7 个冒号
+			ipv6 = addr
+			break
+		}
+	}
+
+	// 将过滤后的地址返回给客户端
 	c.Ctx.WriteString(ipv6)
 }
